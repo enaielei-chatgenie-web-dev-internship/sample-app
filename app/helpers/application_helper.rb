5705e4dc7c87c63accab6f98d7124a3ec501a477
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2022-03-17 10:50:29 +0800

    Add `get_title` helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    BASE_TITLE = "Sample App"
+    def get_title(title="", separator=" | ")
+        if title.empty?()
+            return BASE_TITLE
+        else
+            return "#{BASE_TITLE}#{separator}#{title}"
+        end
+    end
 end
