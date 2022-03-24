f187c72abd89d725a4556f0c650863b39114443a
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2022-03-24 11:17:15 +0800

    User Mailgun instead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,12 +101,11 @@ Rails.application.configure do
   }
 
   ActionMailer::Base.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: "587",
-    authentication: :plain,
-    user_name: ENV["SENDGRID_USERNAME"],
-    password: ENV["SENDGRID_PASSWORD"],
-    domain: "heroku.com",
-    enable_starttls_auto: true,
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'enaielei-chatgenie-sample-app.heroku.com',
+    :authentication => :plain,
   }
 end
