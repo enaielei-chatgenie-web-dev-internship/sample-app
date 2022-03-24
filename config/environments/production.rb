9068d9e59b4bfcf179b35b28ece83dd7945d47fc
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2022-03-24 13:49:03 +0800

    Fix config again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'enaielei-chatgenie-sample-app.herokuapp.com',
+    :domain         => ENV['MAILGUN_DOMAIN'],
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
